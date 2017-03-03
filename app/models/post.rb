6a6b63780b1ed7b1bd5deab67283e991472c53a8
Your Name <you@example.com>
2017-03-03 15:57:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+
+validate :clickbait
+
+def clickbait
+    clickbait_titles= /Won't Believe|Secret|Top \d+|Guess/
+    if !clickbait_titles.match(self.title)
+      errors.add(:title, "Title is not clickbait-y")
+    end
+  end
+
+
+#words.any? { |word| sentence.includes?(word) }
+# baitarray.include(split_title)
+
 end
